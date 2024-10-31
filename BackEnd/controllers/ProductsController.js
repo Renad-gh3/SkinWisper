24470356fcd5923f856dcc163dd9c304baf19479
Joudj7tam <joodtalbz@gmail.com>
2024-10-31 22:07:47 +0300

    backend product schema modyfications

--- BackEnd/controllers/ProductsController.js
+++ BackEnd/controllers/ProductsController.js
@@ -9,7 +9,7 @@ const addProduct = async (req, res) => {
     let image_filename = `${req.file.filename}`;
 
     const Product = new ProductsModel({
-        ID: req.body.ID,
+        id: req.body.ID,
         name: req.body.name,
         description: req.body.description,
         period: req.body.period,
